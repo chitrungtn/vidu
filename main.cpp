a00492c9c561300484a08765cfab3372bee1580b
chitrungtn <tuannl98tn@gmail.com>
2022-03-08 22:54:42 +0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+﻿#include "pch.h"
+#include<iostream>;
+
+using namespace winrt;
+using namespace Windows::Foundation;
+
+int main()
+{
+    init_apartment();
+    Uri uri(L"http://aka.ms/cppwinrt");
+    printf("Hello, %ls!\n", uri.AbsoluteUri().c_str());
+}
